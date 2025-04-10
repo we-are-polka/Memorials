8f33ad700b6c605e6534dd115ed710f4d7cf6c62
we-are-polka <polkawebservices@gmail.com>
2025-04-11 00:52:26 +0300

    Configure unfold admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,16 +1,47 @@
 from django.contrib import admin
 from .models import Memorial, MemorialImage
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import User, Group
 
-class MemorialImageInline(admin.TabularInline):
+# Unfold configuration
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
+from unfold.admin import ModelAdmin, TabularInline
+from unfold.contrib.filters.admin import AutocompleteSelectFilter
+
+
+class MemorialImageInline(TabularInline):
     model = MemorialImage
     extra = 1
 
 @admin.register(Memorial)
-class MemorialAdmin(admin.ModelAdmin):
+class MemorialAdmin(ModelAdmin):
     inlines = [MemorialImageInline]
     list_display = ['name', 'id']
+    list_filter = ['name', 'id']
+    search_fields = ['name','id']
 
 @admin.register(MemorialImage)
-class MemorialImageAdmin(admin.ModelAdmin):
+class MemorialImageAdmin(ModelAdmin):
     list_display = ['memorial', 'order', 'caption']
     list_filter = ['memorial']
+
+
+
+# Unfold user and group config below
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    # Forms loaded from `unfold.forms`
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
--- ndoto/settings.py
+++ ndoto/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Unfold has to start
+    "unfold",
+    "unfold.contrib.filters",
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
